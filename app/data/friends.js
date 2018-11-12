848dfcc557c77c1d176e2cdd1532197d6e3f1e70
MacAttack79 <jmcintosh1979@gmail.com>
2018-11-12 16:19:54 -0500

    update apiRouts to compare arrays and added seed data for friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,19 @@
 var friendsArray = [
   {
-    name: '',
-    photo: '',
-    scores: []
+    name: 'Kevin Costner',
+    photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiHk_Pczs_eAhUrja0KHat8BYQQjRx6BAgBEAU&url=https%3A%2F%2Fwww.biography.com%2Fpeople%2Fkevin-costner-9258776&psig=AOvVaw2JZROV1jpXan9C14MJiLtz&ust=1542138225832638',
+    scores: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      1,
+      2,
+      3,
+      4,
+      5
+    ]
   }
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,49 @@
 var friendsData = require('../data/friends.js')
 
-module.exports = function(app) {
-  app.get('/api/friends', function(req, res) {
+module.exports = function (app) {
+  app.get('/api/friends', function (req, res) {
     res.json(friendsData)
   })
 
-  app.post('/api/friends', function(req, res) {
+  app.post('/api/friends', function (req, res) {
     friendsData.push(req.body)
     res.send(true)
+
+    console.log('\n==================================\n',
+                'New Friend',
+                req.body.name + '\n',
+                req.body.photo + '\n',
+                req.body.scores + '\n',
+                '\n==================================')
+
+    console.log('\n==================================\n',
+                'Existing Friends',
+                friendsData[0].name + '\n',
+                friendsData[0].photo + '\n',
+                friendsData[0].scores + '\n',
+                '\n==================================')
+
+    var newFriendScore = req.body.scores,
+        existingFriendScore = friendsData[0].scores,
+        diffScore = []
+
+    // ** A forEach method to loop through the existing friends to get to their scores.    
+    friendsData.forEach(function (data) {
+      var eachFriendsScore = data.scores
+      console.log(eachFriendsScore)
+    });
+
+
+    // ** For loop to compare the newFriends Score to the Existing Friends Score.  
+    // !! HOW TO DO THIS WHEN THERE ARE MULTIPLE FRIENDS??????????
+
+    for (let i = 0; i < newFriendScore.length; i++) {
+      diffScore.push(Math.abs(newFriendScore[i] - existingFriendScore[i]))
+    };
+
+    console.log(diffScore);
+
+
   })
-  
 }
+
